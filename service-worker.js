df2c23b59a47c079f43d49412614600e1e4fd836
wiaaugusta <wia.augusta@gmail.com>
2026-08-02 13:44:48 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,106 @@
-self.addEventListener("install", () => {
-    console.log("OTP CCTV Installed");
+const CACHE_NAME = "otp-cctv-v1";
+
+const STATIC_FILES = [
+    "./",
+    "./index.html",
+    "./manifest.json",
+    "./icon-192.png",
+    "./icon-512.png"
+];
+
+
+// Install
+
+self.addEventListener(
+"install",
+event => {
+
+console.log(
+"OTP CCTV Installed"
+);
+
+
+event.waitUntil(
+
+caches.open(CACHE_NAME)
+.then(cache => {
+
+return cache.addAll(STATIC_FILES);
+
+})
+
+);
+
+
+self.skipWaiting();
+
+
 });
 
-self.addEventListener("fetch", event => {});
+
+
+
+// Activate
+
+self.addEventListener(
+"activate",
+event => {
+
+
+console.log(
+"OTP CCTV Activated"
+);
+
+
+event.waitUntil(
+
+caches.keys()
+.then(keys => {
+
+
+return Promise.all(
+
+keys
+.filter(key => key !== CACHE_NAME)
+.map(key => caches.delete(key))
+
+);
+
+
+})
+
+
+);
+
+
+self.clients.claim();
+
+
+});
+
+
+
+
+// Fetch
+
+self.addEventListener(
+"fetch",
+event => {
+
+
+event.respondWith(
+
+
+fetch(event.request)
+
+.catch(()=>{
+
+return caches.match(event.request);
+
+})
+
+
+);
+
+
+});
