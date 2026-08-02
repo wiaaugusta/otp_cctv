efc1317a579b26a7e2eacb59d3cae1da725a65c2
wiaaugusta <wia.augusta@gmail.com>
2026-08-02 16:05:04 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,106 +1,36 @@
 const CACHE_NAME = "otp-cctv-v1";
-
 const STATIC_FILES = [
-    "./",
-    "./index.html",
-    "./manifest.json",
-    "./icon-192.png",
-    "./icon-512.png"
+  "/otp_cctv/",
+  "/otp_cctv/index.html",
+  "/otp_cctv/manifest.json",
+  "/otp_cctv/icon-192.png",
+  "/otp_cctv/icon-512.png"
 ];
 
-
-// Install
-
-self.addEventListener(
-"install",
-event => {
-
-console.log(
-"OTP CCTV Installed"
-);
-
-
-event.waitUntil(
-
-caches.open(CACHE_NAME)
-.then(cache => {
-
-return cache.addAll(STATIC_FILES);
-
-})
-
-);
-
-
-self.skipWaiting();
-
-
+self.addEventListener("install", event => {
+  console.log("OTP CCTV Installed");
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(STATIC_FILES))
+  );
+  self.skipWaiting();
 });
 
-
-
-
-// Activate
-
-self.addEventListener(
-"activate",
-event => {
-
-
-console.log(
-"OTP CCTV Activated"
-);
-
-
-event.waitUntil(
-
-caches.keys()
-.then(keys => {
-
-
-return Promise.all(
-
-keys
-.filter(key => key !== CACHE_NAME)
-.map(key => caches.delete(key))
-
-);
-
-
-})
-
-
-);
-
-
-self.clients.claim();
-
-
+self.addEventListener("activate", event => {
+  console.log("OTP CCTV Activated");
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(
+        keys.filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+      ))
+  );
+  self.clients.claim();
 });
 
-
-
-
-// Fetch
-
-self.addEventListener(
-"fetch",
-event => {
-
-
-event.respondWith(
-
-
-fetch(event.request)
-
-.catch(()=>{
-
-return caches.match(event.request);
-
-})
-
-
-);
-
-
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request)
+      .catch(() => caches.match(event.request))
+  );
 });
